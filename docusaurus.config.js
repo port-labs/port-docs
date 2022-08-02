fc0370e4574efb545279337e0ad50cd6ad56be73
Mor Paz <mor@getport.io>
2022-08-02 09:58:18 +0300

    Remove docs from site footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,15 +85,15 @@ const config = {
       footer: {
         style: "dark",
         links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
+          // {
+          //   title: "Docs",
+          //   items: [
+          //     {
+          //       label: "Tutorial",
+          //       to: "/docs/intro",
+          //     },
+          //   ],
+          // },
           {
             title: "Community",
             items: [
