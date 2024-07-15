de6e5100dcbd749f41aa6a3d36501cc6a3ad955c
Noa Sarid <83418206+noaiz@users.noreply.github.com>
2024-07-15 11:08:42 +0300

    Delete resources directory

--- resources/123.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "example" {
-provider = aws.bucket_region
-name = "123"
-acl = "private"
-}
--- resources/321.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "example" {
-provider = aws.bucket_region
-name = "321"
-acl = "private"
-}
