de1387c17de213895be83540acafdb846198db5e
Ozz Shafriri <ozz.shaf@gmail.com>
2024-08-19 15:45:41 +0300

    code adjustment in `Tags` component

--- src/components/guides-section/Tag/Tags.jsx
+++ src/components/guides-section/Tag/Tags.jsx
@@ -1,29 +1,24 @@
-import React, { useMemo } from "react";
+import React from "react";
 import Tag from "/src/components/guides-section/Tag/Tag.jsx";
 import "/src/components/guides-section/styles.css";
 import { Typography } from "@mui/material";
 import { tagsCategoryMap } from "../consts.js";
 
-
 function Tags({ toggleTag }) {
-  const groupedTags = useMemo(
-    () =>
-      Object.entries(tagsCategoryMap).map(([category, tags]) => {
-        return (
-          <div className="guide-tags-category">
-            <Typography className="guide-tags-title">{category}</Typography>
-            <div className="guide-tags-category-content">
-              {tags.map((tag) => (
-                <Tag tag={tag} toggleTag={toggleTag} />
-              ))}
-            </div>
+  return (
+    <div className="guide-tags-container">
+      {Object.entries(tagsCategoryMap).map(([category, tags]) => (
+        <div key={category} className="guide-tags-category">
+          <Typography className="guide-tags-title">{category}</Typography>
+          <div className="guide-tags-category-content">
+            {tags.map((tag) => (
+              <Tag key={tag} tag={tag} toggleTag={toggleTag} />
+            ))}
           </div>
-        );
-      }),
-    []
+        </div>
+      ))}
+    </div>
   );
-
-  return <div className="guide-tags-container">{groupedTags}</div>;
 }
 
 export default Tags;
