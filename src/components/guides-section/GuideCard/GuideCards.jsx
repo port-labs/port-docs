e15d58d42bb02da5a035215834f580be9cb4bd2f
Hadar <hadar@Hadars-MacBook-Pro.local>
2024-08-20 18:25:35 +0300

    empty state and styling

--- src/components/guides-section/GuideCard/GuideCards.jsx
+++ src/components/guides-section/GuideCard/GuideCards.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Typography } from '@mui/material';
 import "/src/components/guides-section/styles.css";
 import GuideCard from './GuideCard';
 
 function GuideCards({ guides }) {
   if (guides.length === 0) {
-    return "No guides found";
+    return <div className="guides-card-empty-state">
+        <img src="/img/guides/icons/Search.svg" width="40px" className="not-zoom" />
+        <Typography className="guides-card-empty-state-title">No guides found</Typography>
+        <Typography className="guides-card-empty-state-body">Try using a different filter or search term</Typography>
+      </div>
   }
 
   return (
--- src/components/guides-section/SearchBar/SearchBar.jsx
+++ src/components/guides-section/SearchBar/SearchBar.jsx
@@ -4,7 +4,7 @@ import "/src/components/guides-section/styles.css";
 function SearchBar({ searchText, setSearchText }) {
   return <div className="search-bar-wrapper">
     <img src="/img/guides/icons/Search.svg" width="24px" className="not-zoom" />
-    <input className="guides-search-bar" placeholder="Search by title, technology (AWS, Jira, etc...), or Port offering (Actions, scorecards, etc...)" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+    <input className="guides-search-bar" placeholder="Search by title or tag (e.g. Slack, Actions, Incident management, etc...)" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
   </div>;
 }
 
