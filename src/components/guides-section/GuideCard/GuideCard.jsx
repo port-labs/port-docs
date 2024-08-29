73e7f2dad9a8e008a3bb35ca102577a0d6e56394
Ozz Shafriri <ozz.shaf@gmail.com>
2024-08-29 13:22:42 +0300

    fix image load

--- src/components/guides-section/GuideCard/GuideCard.jsx
+++ src/components/guides-section/GuideCard/GuideCard.jsx
@@ -1,19 +1,26 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Tag from '/src/components/guides-section/Tag/Tag.jsx'
 import "/src/components/guides-section/styles.css";
-import { GetImage, GetImageLight } from '/src/components/guides-section/Utils.js';
+import { getTagImage } from '/src/components/guides-section/utils.js';
 import { Typography } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+import { useColorMode } from '@docusaurus/theme-common';
 
 function GuideCard({ title, description, tags, logos, category, link }) {
     const history = useHistory();
+    const { isDarkTheme } = useColorMode();
     
     return <div className="guide-card" onClick={()=> history.push(link)}>
         <div className='guide-card-header'>
             <div className='guide-card-header-left'>
                 {logos.map(logo => 
                     <div className='guide-card-header-img-box'>
-                    <img src={GetImage(logo)} onError={(e)=>{e.target.onError = null; e.target.src = GetImageLight(logo)}} width='32px' className='not-zoom'/>
+                        <img 
+                            src={getTagImage(logo, isDarkTheme)} 
+                            onError={(e) => { e.target.src = "/img/guides/icons/" + logo.replace(/ /g, '') + ".svg" }} 
+                            width='32px'
+                            className='not-zoom'
+                        />
                     </div>)
                 }
             </div>
--- src/components/guides-section/Utils.js
+++ src/components/guides-section/Utils.js
@@ -1,17 +1,6 @@
-import { useColorMode } from '@docusaurus/theme-common';
-
-export const GetImageLight = (imgName) => {
-    const imgPathLight = "/img/guides/icons/" + imgName.replace(/ /g, '') + ".svg";
-    return imgPathLight;
-}
-
-export const GetImage = (imgName) => {
-    const { isDarkTheme } = useColorMode();
-    const imgPathDark = "/img/guides/icons/dark/" + imgName.replace(/ /g, '') + ".svg";
-
-    if (!isDarkTheme) {
-        return GetImageLight(imgName);
+export const getTagImage = (tag, isDarkTheme) => {
+    if (isDarkTheme) {
+        return "/img/guides/icons/dark/" + tag.replace(/ /g, '') + ".svg";
     }
-
-    return imgPathDark;
+    return "/img/guides/icons/" + tag.replace(/ /g, '') + ".svg";
 }
