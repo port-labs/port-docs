3f50e9e8de6784719fd0930d145456f3bde3ef97
Ozz Shafriri <ozz.shaf@gmail.com>
2024-08-29 10:02:30 +0300

    reflect selected tags in the query parameters

--- src/components/guides-section/GuidePage/GuidePage.jsx
+++ src/components/guides-section/GuidePage/GuidePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { Typography } from "@mui/material";
 import "/src/components/guides-section/styles.css";
 import Tags from "/src/components/guides-section/Tag/Tags.jsx";
@@ -6,24 +6,35 @@ import SearchBar from "/src/components/guides-section/SearchBar/SearchBar.jsx";
 import GuideCards from "/src/components/guides-section/GuideCard/GuideCards.jsx";
 import { availableGuides } from "../consts.js";
 import ResetIcon from "/static/img/guides/icons/Reset.svg";
+import { useHistory } from "react-router-dom";
 
 function GuidePage() {
+  const history = useHistory();
+
   const [searchText, setSearchText] = useState('');
-  const [selectedTags, setSelectedTags] = useState([]);
+  const paramsTags = new URLSearchParams(location.search).getAll("tags");
+  const [selectedTags, setSelectedTags] = useState(paramsTags);
 
-  const toggleTag = (tag) => {
-    setSelectedTags((prev) =>
-      prev.includes(tag)
+  const toggleTag = useCallback((tag) => {
+    setSelectedTags((prev) => {
+      const newTags = prev.includes(tag)
         ? prev.filter((l) => l !== tag)
-        : [...prev, tag]
-    );
-  };
+        : [...prev, tag];
+      
+      const params = new URLSearchParams(location.search);
+      params.delete("tags");
+      newTags.forEach(tag => params.append("tags", tag));
+      history.push({ search: params.toString() });
 
-  const filteredGuides = availableGuides.filter((guide) => {
+      return newTags;
+    });
+  }, [history]);
+
+  const filteredGuides = useMemo(() => availableGuides.filter((guide) => {
     const matchesTag = selectedTags.length === 0 || selectedTags.every((tag) => guide.tags.includes(tag) || guide.logos.includes(tag) || guide.additionalTags && guide.additionalTags.includes(tag));
     const matchesSearch = guide.title.toLowerCase().includes(searchText.toLowerCase()) || guide.description.toLowerCase().includes(searchText.toLowerCase()) ;
     return matchesTag && matchesSearch;
-  });
+  }), [selectedTags, searchText]);
 
   return (
     <>
