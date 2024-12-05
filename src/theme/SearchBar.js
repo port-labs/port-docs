a31154725673014c4c08be513454ded16923d442
Hadar <hadar@getport.io>
2024-12-05 15:49:17 +0200

    add kapa modal to docs

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -6,8 +6,10 @@ export default function SearchBarWrapper(props) {
   return (
     <div className="custom-search-bar">
       <SearchBar {...props} />
-      <Typography position={"relative"} fontSize={"small"} fontFamily="Space Grotesk" fontWeight='500'>OR</Typography>
-      <button className="ask-kapa-button"><Typography color='white' fontFamily="Space Grotesk" fontWeight='500' fontSize="15px">✨ Ask AI</Typography></button>
+      {/* <Typography position={"relative"} fontSize={"small"} fontFamily="Space Grotesk" fontWeight='500'>OR</Typography> */}
+      <button className="ask-kapa-button">
+        <Typography className='ask-kapa-button-text' color='white' fontFamily="Space Grotesk" fontWeight='500' fontSize="15px">✨ Ask AI</Typography>
+      </button>
     </div>
   );
 }
