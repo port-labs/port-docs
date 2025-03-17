0194bef764c05d3745ea58f6aea55bbd5fcb0802
Matan Grady <matan.grady@getport.io>
2025-03-17 23:13:14 +0200

    fix design of closed beta

--- src/components/ClosedBetaFeatureNotice/index.jsx
+++ src/components/ClosedBetaFeatureNotice/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import Markdown from "@theme/Markdown";
+import Admonition from '@theme/Admonition';
 
 const notices = {
-  aiAgents:
-    ':::info Closed Beta Feature\n' +
-    'This feature is currently in closed beta with limited availability. Access is provided on an application basis.\n\n' +
-    'To request access, please reach out to us by filling [this form](https://forms.gle/BygmbCWcf1Vy4KPW9).\n' +
-    ':::',
-  default:
-    ':::info Closed Beta Feature\n' +
-    'This feature is currently in closed beta with limited availability. Access is provided on an application basis.\n' +
-    ':::'
+  aiAgents: {
+    content: <>
+      This feature is currently in closed beta with limited availability. Access is provided on an application basis.
+      <br/><br/>
+      To request access, please reach out to us by filling <a href="https://forms.gle/BygmbCWcf1Vy4KPW9">this form</a>.
+    </>
+  },
+  default: {
+    content: <>
+      This feature is currently in closed beta with limited availability. Access is provided on an application basis.
+    </>
+  }
 };
 
 const ClosedBetaFeatureNotice = ({ id }) => {
+  const notice = notices[id] || notices.default;
+  
   return (
-    <Markdown>
-      {notices[id] || notices.default}
-    </Markdown>
+    <Admonition type="info" title="Closed Beta Feature">
+      {notice.content}
+    </Admonition>
   );
 };
 
