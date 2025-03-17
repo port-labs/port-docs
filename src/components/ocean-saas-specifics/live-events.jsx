73cf80197c68ac4a1238d3d4747447557952743d
Hadar <hadar@getport.io>
2025-03-17 19:03:57 +0200

    fix live events template and syntax

--- src/components/ocean-saas-specifics/live-events.jsx
+++ src/components/ocean-saas-specifics/live-events.jsx
@@ -1,89 +1,91 @@
 import Markdown from "@theme/Markdown";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
 
 const defaultLiveEvents = {
   unsupported:
-    `Currently, live events are not supported for integrations hosted by Port.<br/> \
-    Resyncs will be performed **periodically** (with a configurable interval), or **manually** triggered by you via Port's UI.<br/> \
-    <br/> \ 
-    Therefore, real-time events (including GitOps) will not be ingested into Port immediately.<br/>\
-    Live events support for this integration is WIP and will be supported in the near future.`,
+    `Currently, live events are not supported for integrations hosted by Port. \
+    \nResyncs will be performed **periodically** (with a configurable interval), or **manually** triggered by you via Port's UI. \
+    \n\nTherefore, real-time events (including GitOps) will not be ingested into Port immediately. \
+    \nLive events support for this integration is WIP and will be supported in the near future.`,
   supported:
     'This integration supports live events, allowing real-time updates to your software catalog without waiting for the next scheduled sync.',
 };
 
-const liveEvents = {
-  Jira: '**Supported event triggers for user-token based installations:** \
-    \n- Issue: \
-    \n\t- jira:issue_created \
-    \n\t- jira:issue_updated \
-    \n\t- jira:issue_deleted \
-    \n- Project: \
-    \n\t- project_created \
-    \n\t- project_updated \
-    \n\t- project_deleted \
-    \n- User: \
-    \n\t- user_created \
-    \n\t- user_updated \
-    \n\t- user_deleted',
+export const liveEvents = {
+  Jira:
+    '**Issue:** \
+    \n- jira:issue_created \
+    \n- jira:issue_updated \
+    \n- jira:issue_deleted \
+    \n\n**Project:** \
+    \n- project_created \
+    \n- project_updated \
+    \n- project_deleted \
+    \n\n**User:** \
+    \n- user_created \
+    \n- user_updated \
+    \n- user_deleted',
   Jira_OAuth:
-    '\n\n**Supported event triggers for OAuth based installations:** \
-    \n- Issue: \
-    \n\t- jira:issue_created \
-    \n\t- jira:issue_updated \
-    \n\t- jira:issue_deleted',
-  Snyk: '**Supported event triggers:** \
-    \n- Project related events \
+    '**Issue:** \
+    \n- jira:issue_created \
+    \n- jira:issue_updated \
+    \n- jira:issue_deleted',
+  Snyk:
+   '\n- Project related events \
     \n- Target related events',
   PagerDuty:
-    '**Supported event triggers:** \
-    \n- Service: \
-    \n\t- service.created \
-    \n\t- service.deleted \
-    \n\t- service.updated \
-    \n- Incident: \
-    \n\t- incident.acknowledged \
-    \n\t- incident.annotated \
-    \n\t- incident.delegated \
-    \n\t- incident.escalated \
-    \n\t- incident.priority_updated \
-    \n\t- incident.reassigned \
-    \n\t- incident.reopened \
-    \n\t- incident.resolved \
-    \n\t- incident.status_update_published \
-    \n\t- incident.responder.added \
-    \n\t- incident.responder.replied \
-    \n\t- incident.triggered \
-    \n\t- incident.unacknowledged',
+    '**Service:** \
+    \n- service.created \
+    \n- service.deleted \
+    \n- service.updated \
+    \n\n**Incident:** \
+    \n- incident.acknowledged \
+    \n- incident.annotated \
+    \n- incident.delegated \
+    \n- incident.escalated \
+    \n- incident.priority_updated \
+    \n- incident.reassigned \
+    \n- incident.reopened \
+    \n- incident.resolved \
+    \n- incident.status_update_published \
+    \n- incident.responder.added \
+    \n- incident.responder.replied \
+    \n- incident.triggered \
+    \n- incident.unacknowledged',
   PagerDuty_OAuth:
-    '**Supported event triggers:** \
-    \n- Service: \
-    \n\t- service.created \
-    \n\t- service.deleted \
-    \n\t- service.updated \
-    \n- Incident: \
-    \n\t- incident.acknowledged \
-    \n\t- incident.annotated \
-    \n\t- incident.delegated \
-    \n\t- incident.escalated \
-    \n\t- incident.priority_updated \
-    \n\t- incident.reassigned \
-    \n\t- incident.reopened \
-    \n\t- incident.resolved \
-    \n\t- incident.status_update_published \
-    \n\t- incident.responder.added \
-    \n\t- incident.responder.replied \
-    \n\t- incident.triggered \
-    \n\t- incident.unacknowledged',
+    '**Service:** \
+    \n- service.created \
+    \n- service.deleted \
+    \n- service.updated \
+    \n\n**Incident:** \
+    \n- incident.acknowledged \
+    \n- incident.annotated \
+    \n- incident.delegated \
+    \n- incident.escalated \
+    \n- incident.priority_updated \
+    \n- incident.reassigned \
+    \n- incident.reopened \
+    \n- incident.resolved \
+    \n- incident.status_update_published \
+    \n- incident.responder.added \
+    \n- incident.responder.replied \
+    \n- incident.triggered \
+    \n- incident.unacknowledged',
 };
 
-export const OceanSaasLiveEventsTriggers = ({ id, isOAuth = false }) => {
+export const OceanSaasLiveEventsTriggersOAuth = ({ id, isOAuth = false }) => {
   return (
     <Markdown>
-      {liveEvents[id]
-        ? isOAuth
-          ? `\n\n<details><summary><b>Supported live event triggers</b></summary>\n\n${liveEvents[id + "_OAuth"]}\n\n</details><br></br>`
-          : `\n\n<details><summary><b>Supported live event triggers</b></summary>\n\n${liveEvents[id]}\n\n</details><br></br>`
-        : ""}
+    {liveEvents[id + "_OAuth"]}
+    </Markdown>
+  );
+};
+
+export const OceanSaasLiveEventsTriggersManual = ({ id, isOAuth = false }) => {
+  return (
+    <Markdown>
+    {liveEvents[id]}
     </Markdown>
   );
 };
