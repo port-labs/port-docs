c2ba5be2125f9428278d1ec3379f90a73157a193
mayam-port <mayam@getport.io>
2025-04-06 13:33:19 +0300

    Update consts.js create-pagerduty-incident-from-newrelic-alert

--- src/components/guides-section/consts.js
+++ src/components/guides-section/consts.js
@@ -936,5 +936,12 @@ export const availableGuides = [
         tags: [],
         logos: ["Webhook"],
         link: "/guides/all/manage-integrations",
+    },
+    {
+        title: "Create a PagerDuty incident from a New Relic alert",
+        description: "Create a self-service action that creates a PagerDuty incident from a New Relic alert",
+        tags: ["PagerDuty","New Relic","Actions","Webhook"],
+        logos: ["PagerDuty","New Relic"],
+        link: "/guides/all/create-pagerduty-incident-from-newrelic-alert.md",
     }
 ]
