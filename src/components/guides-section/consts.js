ecc4dd5c1350e17e7673bca13200dfc559efcd07
Pages Coffie <isaac.p.coffie@gmail.com>
2025-05-05 21:27:15 +0000

    approval workflow

--- src/components/guides-section/consts.js
+++ src/components/guides-section/consts.js
@@ -1061,7 +1061,7 @@ export const availableGuides = [
     {
         title: "Notify users upon approval of self-service actions",
         description: "Create an automation that notifies users when a self-service action is approved",
-        tags: ["Approval workflow", "Actions", "Webhook", "Automations"],
+        tags: ["Approval workflow", "Actions", "Automations", "Webhook"],
         logos: ["Webhook"],
         link: "/guides/all/notify-users-upon-approval-of-action",
     }
