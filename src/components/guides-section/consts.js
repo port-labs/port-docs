89b0fc629f3efe245b096b928cc682b1eec0fb29
Pages Coffie <isaac.p.coffie@gmail.com>
2025-06-03 07:27:49 +0000

    EC2 management experience in Port

--- src/components/guides-section/consts.js
+++ src/components/guides-section/consts.js
@@ -1178,6 +1178,13 @@ export const availableGuides = [
         tags: ["Engineering metrics", "Webhook"],
         logos: ["Webhook"],
         link: "/guides/all/send-typeform-survey-results-to-port",
+    },
+    {
+        title: "Visualize and manage your EC2 instances",
+        description: "Create a dashboard that visualizes and manages your AWS EC2 instances",
+        tags: ["Cloud access management", "AWS", "GitHub", "Dashboards"],
+        logos: ["AWS", "GitHub"],
+        link: "/guides/all/visualize-and-manage-aws-ec2-instances",
     }
 ]
 
