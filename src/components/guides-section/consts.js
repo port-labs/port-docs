8814fdd2c12d3c5ccc9e9964eb0c824ef3e68cf3
PagesCoffy <isaac.p.coffie@gmail.com>
2025-12-22 11:17:58 +0000

    Update src/components/guides-section/consts.js
    Co-authored-by: sivanel97 <sivane@getport.io>

--- src/components/guides-section/consts.js
+++ src/components/guides-section/consts.js
@@ -1595,7 +1595,7 @@ export const availableGuides = [
     },
     {
         title: "Gain visibility into delivery performance",
-        description: "Measure and track delivery performance as part of your engineering intelligence framework using key metrics like PR cycle time, PR throughput, deployment frequency, and overdue PRs",
+        description: "Track engineering delivery performance using key metrics like PR cycle time, throughput, deployment frequency, and overdue PRs",
         tags: ["Engineering Intelligence", "GitHub", "Dashboards"],
         logos: ["GitHub"],
         link: "/guides/all/measure-and-track-delivery-performance",
